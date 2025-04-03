e5c44740eaff4a53ad042cd332706c1e8bd87105
Dmitriy Fitisov <dmitriy@radier.ca>
2025-04-03 10:32:09 -0400

    Added handling for Favorite button

--- Landmarks/Vews/Landmarks/LandmarkRow.swift
+++ Landmarks/Vews/Landmarks/LandmarkRow.swift
@@ -33,12 +33,14 @@ struct LandmarkRow: View {
 
 
 #Preview("Turtle Rock") {
+    let landmarks = ModelData().landmarks
     Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
 }
 #Preview("Salmon") {
+    let landmarks = ModelData().landmarks
     LandmarkRow(landmark: landmarks[1])
 }
 
